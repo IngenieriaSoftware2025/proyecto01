c282e6f819d3269c15171797cf2fdf3277ab1f82
Guzmán285 <herberthguzman0@gmail.com>
2025-06-10 08:21:52 -0600

    inventario

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -6,7 +6,7 @@ use MVC\Router;
 
 class AppController {
     public static function index(Router $router){
-        $router->render('pages/index', []);
+        $router->render('login/index', []);
     }
 
 }
